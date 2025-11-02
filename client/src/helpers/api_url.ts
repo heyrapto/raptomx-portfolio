17ffd067c87d3536bff4b36b548af8c281f0377d
heyrapto <kalejaiyecaleb@gmail.com>
2025-11-02 16:11:30 +0100

    Set production flag to true and update API_URL for deployment

--- client/src/helpers/api_url.ts
+++ client/src/helpers/api_url.ts
@@ -1,2 +1,2 @@
-const prod = false;
-export const API_URL = prod ? "https://raptomx-api.pxxl.click/api" : 'http://localhost:5800/api';
+const prod = true;
+export const API_URL = prod ? "https://raptomx-portfolio-server.onrender.com/api" : 'http://localhost:5800/api';
